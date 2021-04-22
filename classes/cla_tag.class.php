415494af847827d6cad87be4d0d8a04581f6a148
Maxime Beacco <Obscure-Angel@gmx.fr>
2021-04-22 23:16:46 +0200

    Ajout de classes balise

---
+++ classes/cla_tag.class.php
@@ -0,0 +1,21 @@
+<?php
+
+abstract class Tag {
+	
+	/**
+	 * @var array
+	 */
+	protected $ca_attributes;
+
+	/**
+	 * @var array
+	 */
+	protected $ca_style;
+
+	/**
+	 * @var array
+	 */
+	protected $ca_childs;
+
+
+}
